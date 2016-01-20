3ee0d73591257cf57a1cefa17a0570d70ea5f59a
LYP951018 <liuyupei951018@hotmail.com>
2016-01-21 00:17:51 +0800

    remove useless stuff

--- Demo/AnimationBasis/Common.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "..\..\Source\Gui\NativeWindows.h"
-#include "..\..\Source\Gui\Shapes.h"
-#include "..\..\Source\Gui\Panel.h"
-#include <UIAnimation.h>
-
-//#pragma comment(lib,"YupeiLibrary.lib")
