e678030f42a22a9b5d28280c2bb97b4847182c7f
刘雨培 <liuyupei951018@hotmail.com>
2016-04-23 10:56:07 +0800

    Format.

--- Source/Library/ConstructDestruct.hpp
+++ Source/Library/ConstructDestruct.hpp
@@ -1,52 +1,52 @@
 #pragma once
 
+#include "Ranges\Xrange.hpp"
 #include <utility>  //for std::forward
 #include <type_traits>
-#include "Ranges\Xrange.hpp"
 
 namespace Yupei
 {
-	template<typename ObjectT, typename... ParamsT>
+    template<typename ObjectT, typename... ParamsT>
     void construct(ObjectT* ptr, ParamsT&&... params) noexcept(std::is_nothrow_constructible<ObjectT, ParamsT&&...>::value)
-	{
-		::new (static_cast<void*>(ptr)) ObjectT(std::forward<ParamsT>(params)...);
-	}
+    {
+        ::new (static_cast<void*>(ptr)) ObjectT(std::forward<ParamsT>(params)...);
+    }
 
-	template<typename ObjectT>
-	void destroy(const ObjectT* ptr) noexcept
-	{
-		ptr->~ObjectT();
-	}
+    template<typename ObjectT>
+    void destroy(const ObjectT* ptr) noexcept
+    {
+        ptr->~ObjectT();
+    }
 
-	namespace Internal
-	{
-		template<typename ObjectT>
-		void DestroyNImp(ObjectT* ptr, std::size_t count, std::true_type) noexcept
-		{
-			;//no-op
-		}
+    namespace Internal
+    {
+        template<typename ObjectT>
+        void DestroyNImp(ObjectT* ptr, std::size_t count, std::true_type) noexcept
+        {
+            ;//no-op
+        }
 
-		template<typename ObjectT>
-		void DestroyNImp(ObjectT* ptr, std::size_t count, std::false_type) noexcept
-		{
-			for (std::size_t i{};i < count;++i)
-				Yupei::destroy(ptr + i);
-		}
-	}
+        template<typename ObjectT>
+        void DestroyNImp(ObjectT* ptr, std::size_t count, std::false_type) noexcept
+        {
+            for (std::size_t i {};i < count;++i)
+                Yupei::destroy(ptr + i);
+        }
+    }
 
-	template<typename ObjectT>
-	void destroy_n(ObjectT* ptr, std::size_t count) noexcept
-	{
-		Internal::DestroyNImp(ptr, count, std::is_trivially_destructible<ObjectT>());
-	}
+    template<typename ObjectT>
+    void destroy_n(ObjectT* ptr, std::size_t count) noexcept
+    {
+        Internal::DestroyNImp(ptr, count, std::is_trivially_destructible<ObjectT>());
+    }
 
-	template<typename ObjectT,typename... ParamsT>
-	ObjectT* construct_n(ObjectT* ptr, std::size_t count, ParamsT&&... param) noexcept(std::is_nothrow_constructible<ObjectT, ParamsT&&...>::value)
-	{
-		for(auto i : xrange(count))
-			(void)Yupei::construct(ptr + i, std::forward<ParamsT>(param)...);
+    template<typename ObjectT, typename... ParamsT>
+    ObjectT* construct_n(ObjectT* ptr, std::size_t count, ParamsT&&... param) noexcept(std::is_nothrow_constructible<ObjectT, ParamsT&&...>::value)
+    {
+        for (auto i : xrange(count))
+            (void)Yupei::construct(ptr + i, std::forward<ParamsT>(param)...);
         return ptr + count;
-	}
+    }
 }
 
 
